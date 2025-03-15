5b33407ac4359f9fc69fe2b4c03d30bfa1b4c035
sqfasle <113459188+sqfasle@users.noreply.github.com>
2025-03-14 23:47:59 -0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        const section = document.querySelector(this.getAttribute('href'));
-        section.scrollIntoView({ behavior: 'smooth' });
-    });
-});
